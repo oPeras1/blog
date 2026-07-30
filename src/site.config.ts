da7f5f62864433d5deb15dc0b06d0480ba50662b
Hugo de Sousa Pereira <hugo.s.pereira@tecnico.ulisboa.pt>
2026-07-30 19:36:04 +0100

    feat: add CV link to menu navigation

--- src/site.config.ts
+++ src/site.config.ts
@@ -48,6 +48,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/notes/",
 		title: "Notes",
 	},
+	{
+		path: "https://operas.pt/CV.pdf",
+		title: "CV",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
